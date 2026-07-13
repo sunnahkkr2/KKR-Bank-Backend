15c8165de9e034b1e73b59441d7c70a8ee1e3b07
sunnahkkr2 <sunnahkkr@gmail.com>
2026-07-12 22:59:39 -0700

    Debug MongoDB

--- config/db.js
+++ config/db.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to MongoDB...");
+    console.log("URI exists:", !!process.env.MONGODB_URI);
+
     await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB Connection Error:", error.message);
+    console.error(error);
     process.exit(1);
   }
 };
